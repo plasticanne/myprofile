62c89c6f375993ce43410a2b827e899ea26d53b6
plasticanne <plastic.anne@gmail.com>
2016-09-14 13:43:32 +0800

    loading screen videoFormatCheck video background

--- js/pagerun.js
+++ js/pagerun.js
@@ -11,24 +11,50 @@ var pagerun;
     $("#fullpage").fullpage({
         anchors: ['sectiona0', 'sectiona1', 'sectiona2', 'sectiona3'],
         menu: '#Scrollspy',
-        afterLoad: function(index, nextindex, direction, aaaaaa) {
+        afterLoad: function(anchorLink, index) {
             //                var screenh = $(window).height();
             //                $("#section1").css("background-position", "0px " + screenh + "px");
-            if ($('#aaa').css("marginTop") != leftdiv[nextindex - 1]) {
+            if ($('#aaa').css("marginTop") != leftdiv[index - 1]) {
                 $('#aaa').velocity("stop", true).velocity({
-                    marginTop: leftdiv[nextindex - 1] + "px"
+                    marginTop: leftdiv[index - 1] + "px"
                 }, 800)
             };
-            //            $('#Standby1')[0].play();
+            if (anchorLink == 'sectiona0') {
+                $('#video1')[0].play();
+            };
         },
-        //       afterSlideLoad: function(anchorLink, index, slideIndex, direction,bbbbbb) {
-        //           $('#Standby1')[0].play();
-        //      },
-        //       afterRender: function(){
-        //          $('#Standby1')[0].play();
-        //       },
-        //       afterResize: function() {
-        //           $('#Standby1')[0].play();
-        //       },
+        //        afterSlideLoad: function(anchorLink, index, slideIndex, direction, bbbbbb) {
+        //            $('#video1')[0].play();
+        //        },
+        //        afterRender: function() {
+        //            $('#video1')[0].play();
+        //        },
+        //        afterResize: function() {
+        //            $('#video1')[0].play();
+        //        },
     });
 });
+
+function videoFormatCheck() {
+    var testEl = document.createElement("video"),
+        mpeg4, h264, ogg, webm;
+    if (testEl.canPlayType) {
+        // Check for MPEG-4 support
+        mpeg4 = "" !== testEl.canPlayType('video/mp4; codecs="mp4v.20.8"');
+
+        // Check for h264 support
+        h264 = "" !== (testEl.canPlayType('video/mp4; codecs="avc1.42E01E"') || testEl.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"'));
+
+        // Check for Ogg support
+        ogg = "" !== testEl.canPlayType('video/ogg; codecs="theora"');
+
+        // Check for Webm support
+        webm = "" !== testEl.canPlayType('video/webm; codecs="vp8, vorbis"');
+    }
+    return {
+        "mpeg4": mpeg4,
+        "h264": h264,
+        "ogg": ogg,
+        "webm": webm
+    }
+};
