dcf5ae97659b16fc8b0e52db73151e18393f7fd3
plasticanne <plastic.anne@gmail.com>
2016-09-18 14:58:55 +0800

    css perfixfree smil --> Snap.svg

--- js/svgrun.js
+++ js/svgrun.js
@@ -1,13 +1,49 @@
 var svgrun;
-
+var svgoff;
 (svgrun = function() {
     $('#logo').load('images/logo.svg', null, function() {
-        //	$("#logo").attr("src", "images/logo.svg", function() {
+        /*
+                var svgpath0 = document.getElementById("svgline0");
+                var svgpath1 = document.getElementById("svgline1");
+                var svglength0 = svgpath0.getTotalLength();
+                var svglength1 = svgpath1.getTotalLength();*/
+        //    console.log(svglength0, svglength1)
 
-        var svgpath0 = document.getElementById("svgline0");
-        var svgpath1 = document.getElementById("svgline1");
-        var svglength0 = svgpath0.getTotalLength();
-        var svglength1 = svgpath1.getTotalLength();
-    //    console.log(svglength0, svglength1)
+        var grad_10 = [{
+            'x1': 2
+        }, {
+            'x1': 1
+        }];
+        var grad_11 = [{
+
+            'cx': 1,
+            'cy': 0.9,
+        }, {
+            'cx': 0.5,
+            'cy': 0.2,
+        }, {
+            'cx': 0,
+            'cy': 0.9
+
+        }];
+var grad11=Snap("#grad_11");
+var grad10=Snap("#grad_10");
+        (function animateCircle(i) {
+            grad11.animate(grad_11[i], 1000, function() {
+                animateCircle(++i in grad_11 ? i : 0);
+            });
+        })(0);
+        (function animateCircle(i) {
+            grad10.animate(grad_10[i], 1000, function() {
+                animateCircle(++i in grad_10 ? i : 0);
+            });
+        })(0);
     });
+
+
+
+
+});
+(svgoff = function() {
+    Snap("#logo1").remove()
 });
